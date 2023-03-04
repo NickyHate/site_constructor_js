1c347751162207b48b3a0bd4415ae78d86350dff
Nikita <nikitostokarev22@gmail.com>
2023-03-04 12:32:43 +0300

    change model, add function css, optimize code

--- src/model.js
+++ src/model.js
@@ -5,23 +5,50 @@ export const model = [
         value: 'Конструктор сайтов на чистом JavaScript',
         options: {
             tag: 'h2',
-            styles: `background: linear-gradient(to right, #ff0099, #493240); color: #fff; text-align: center; padding: 1.5rem;`
+            styles: {
+                background: 'linear-gradient(to right, #ff0099, #493240)',
+                color: '#fff',
+                padding: '1.5rem',
+                'text-align': 'center'
+            }
         }
     },
     {
-        type: 'text',
-        value: 'Some text'
+        type: 'image',
+        value: image,
+        options: {
+            styles: {
+                padding: '2rem 0',
+                display: 'flex',
+                'justify-content': 'center'
+            }
+        }
     },
     {
         type: 'columns',
         value: [
-            '111111',
-            '222222',
-            '333333'
-        ]
+            'Приложение на чистом JS, без библиотек',
+            'Учим работу принципов SOLID и ООП в JS',
+            'Создаём UI своими руками'
+        ],
+        options: {
+            styles: {
+                background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+                padding: '2rem',
+                color: '#fff',
+                'font-weight': 'bold'
+            }
+        }
     },
     {
-        type: 'image',
-        value: image
+        type: 'text',
+        value: 'Some text',
+        options: {
+            styles: {
+                background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+                padding: '1rem',
+                'font-weight': 'bold'
+            }
+        }
     }
 ]
