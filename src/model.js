dd8244cdd57741a3c00c325eeb7b20e2da6a6b2f
Nikita <nikitostokarev22@gmail.com>
2023-03-04 12:55:09 +0300

    add class Block, optimize model

--- src/model.js
+++ src/model.js
@@ -1,54 +1,45 @@
-import image from "./assets/image.png"
+import image from "./assets/image3.jpg"
+import {Block} from "./classes/blocks";
+
 export const model = [
-    {
-        type: 'title',
-        value: 'Конструктор сайтов на чистом JavaScript',
-        options: {
-            tag: 'h2',
-            styles: {
-                background: 'linear-gradient(to right, #ff0099, #493240)',
-                color: '#fff',
-                padding: '1.5rem',
-                'text-align': 'center'
-            }
+    new Block('title', 'Конструктор сайтов на чистом JavaScript', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
         }
-    },
-    {
-        type: 'image',
-        value: image,
-        options: {
-            styles: {
-                padding: '2rem 0',
-                display: 'flex',
-                'justify-content': 'center'
-            }
+    }),
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'Тут должна быть картинка'
+    }),
+    new Block('columns', [
+        'Приложение на чистом JS, без библиотек',
+        'Учим работу принципов SOLID и ООП в JS',
+        'Создаём UI своими руками'
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
         }
-    },
-    {
-        type: 'columns',
-        value: [
-            'Приложение на чистом JS, без библиотек',
-            'Учим работу принципов SOLID и ООП в JS',
-            'Создаём UI своими руками'
-        ],
-        options: {
-            styles: {
-                background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
-                padding: '2rem',
-                color: '#fff',
-                'font-weight': 'bold'
-            }
+    }),
+    new Block('text', 'Some text', {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            padding: '1rem',
+            'font-weight': 'bold'
         }
-    },
-    {
-        type: 'text',
-        value: 'Some text',
-        options: {
-            styles: {
-                background: 'linear-gradient(to left, #f2994a, #f2c94c)',
-                padding: '1rem',
-                'font-weight': 'bold'
-            }
-        }
-    }
+    })
 ]
